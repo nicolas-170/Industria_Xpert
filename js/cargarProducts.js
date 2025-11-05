b8c0b6bff57b95d6b43fbe02b22f7947505104fa
Admin <nicolas.pc.170@gmail.com>
2025-11-04 22:03:25 -0500

    fix: Update image base path to ensure correct image loading

--- js/cargarProducts.js
+++ js/cargarProducts.js
@@ -115,13 +115,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     function imagenProducto(nombreProducto) {
         // Extensiones posibles
         const extensiones = ['png', 'jpg', 'jpeg'];
-        const basePath = './assets/img/';
+        const basePath = '/assets/img/';
         nombreProducto = nombreProducto.trim();
         
         // Retorna una Promesa porque se usa fetch
         return new Promise(async (resolve) => {
             for (const ext of extensiones) {
                 const ruta = `${basePath}${nombreProducto}.${ext}`;
+                console.log(window.location.origin + ruta)
                 try {
                     // Verificamos si existe la imagen sin generar error en consola
                     const response = await fetch(ruta, { method: 'HEAD' });
